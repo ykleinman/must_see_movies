77f7aa76355b68a98d50b6f51b164aff23eea269
Raghu Betina <raghu@firstdraft.com>
2016-11-15 14:24:32 +0000

    Whitelist columns in Director for ActiveAdmin

--- app/admin/director.rb
+++ app/admin/director.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Director do
 
+ permit_params :name, :bio, :dob, :image_url
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
